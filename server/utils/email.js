0acd72fa48a7d5d876b7318e95869efd36780479
Zainab-Nadhira <zainabnadhira005@gmail.com>
2026-03-17 13:26:35 +0530

    Switch to Brevo email

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,9 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || 'smtp.gmail.com',
-  port: process.env.EMAIL_PORT || 465,
-  secure: true,
+  host: process.env.EMAIL_HOST,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
